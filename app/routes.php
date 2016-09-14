feb36b18be2c6c8d7f099d0a24c821c3a8fd0301
delolivier77 <delolivier77@live.fr>
2016-09-14 12:52:30 +0200

    création route inscription etudiant

--- app/routes.php
+++ app/routes.php
@@ -2,4 +2,7 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
+
+		/* inscription utilisateur dans la BDD*/
+		['GET', '/inscription_etudiant', 'User#inscriptionEtudiant', 'user_inscription_etudiant'],
 	);
