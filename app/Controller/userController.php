e33289ce4f8a442983b76b9a9e33392195098fed
delolivier77 <delolivier77@live.fr>
2016-09-14 12:55:01 +0200

    ajout userController.php

---
+++ app/Controller/userController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Controller;
+
+use \W\Controller\Controller;
+use \W\Model\UsersModel;
+use \W\Security\AuthentificationModel;
+
+class UserController extends Controller
+{}
+
+
+?>
