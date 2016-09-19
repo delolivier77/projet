b2bacc9756a7ed15e0440c7c1934e1ad8b346266
delolivier77 <delolivier77@live.fr>
2016-09-19 16:25:24 +0200

    Modification des inscriptions

--- app/Controller/userController.php
+++ app/Controller/userController.php
@@ -42,6 +42,79 @@ class UserController extends Controller
 		$scolarite = $scolariteModel->findAllScolarite();
 		
 
+
+
+
+
+
+		$civilite_h = (!isset($_POST['civilite']) || (isset($_POST['civilite'])) && $_POST['civilite'] == "M.") ? 'checked' : "";  
+		$civilite_f = (isset($_POST['civilite']) && $_POST['civilite'] == "Mme") ? 'checked' : "";  
+		$nom = (isset($_POST['nom'])) ? $_POST['nom'] : "";
+		$prenom = (isset($_POST['prenom'])) ? $_POST['prenom'] : "";
+		$email = (isset($_POST['email'])) ? $_POST['email'] : "";
+		$mdp = (isset($_POST['mdp'])) ? $_POST['mdp'] : "";
+		$date_naissance = (isset($_POST['date_naissance'])) ? $_POST['date_naissance'] : "";
+		$tel = (isset($_POST['tel'])) ? $_POST['tel'] : "";
+		$adresse = (isset($_POST['adresse'])) ? $_POST['adresse'] : "";
+		$cp = (isset($_POST['cp'])) ? $_POST['cp'] : "";
+		$ville = (isset($_POST['ville'])) ? $_POST['ville'] : "";
+
+
+		$b1 = ""; $b2 = "" ; $b3 = "" ; $b4 = "" ; $b5 = "";
+		if (isset($_POST['niveau_etude']))
+		{
+			switch ($_POST['niveau_etude'])
+		    {
+		      case 'Bac +1':
+		        $b1 = "selected";
+		        break;
+		      case 'Bac +2':
+		        $b2 = "selected";
+		        break;
+		      case 'Bac +3':
+		        $b3 = "selected";
+		        break;
+		      case 'Bac +4':
+		        $b4 = "selected";
+		        break;
+		      case 'Bac +5 (ou +)':
+		        $b5 = "selected";
+		        break;
+		    }
+		}
+
+		$num_etudiant = (isset($_POST['num_etudiant'])) ? $_POST['num_etudiant'] : "";
+
+		$rdv_ff = (!isset($_POST['type_rdv']) || (isset($_POST['type_rdv'])) && $_POST['type_rdv'] == "faceface") ? 'checked' : "";  
+		$rdv_w = (isset($_POST['type_rdv']) && $_POST['type_rdv'] == "webcam") ? 'checked' : "";
+		$rdv_2 = (isset($_POST['type_rdv']) && $_POST['type_rdv'] == "both") ? 'checked' : "";
+
+		$description = (isset($_POST['description'])) ? $_POST['description'] : "";
+		$detail_dispo = (isset($_POST['detail_dispo'])) ? $_POST['detail_dispo'] : "";
+		$tarif = (isset($_POST['tarif'])) ? $_POST['tarif'] : "";
+
+
+
+		$tab = array('civilite_h' => $civilite_h, 'civilite_f' => $civilite_f, 'nom' => $nom, 'prenom' => $prenom);
+
+		debug ($tab);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		if(empty($_POST['photo']) || empty($_POST['civilite']) || empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['mdp']) || empty($_POST['date_naissance']) || empty($_POST['tel']) || empty($_POST['adresse']) || empty($_POST['cp']) || empty($_POST['ville']) || empty($_POST['niveau_etude']) || empty($_POST['num_etudiant']) || empty($_POST['matiere']) || empty($_POST['classe_debut']) || empty($_POST['classe_fin']) || empty($_POST['description']) || empty($_POST['description']) || empty($_POST['type_rdv']) || empty($_POST['tarif']))
 		{
 			$message .= "Tous les champs sont obligatoires <br>";
@@ -104,7 +177,7 @@ class UserController extends Controller
 		}
 		else
 		{
-			$this->show('user/inscription_etudiant', ['matiere_list' => $matiere, 'scolarite_list' => $scolarite, 'message' => $message]);
+			$this->show('user/inscription_etudiant', ['matiere_list' => $matiere, 'scolarite_list' => $scolarite, 'message' => $message, 'tab' => $tab]);
 		}
 		
 	}
