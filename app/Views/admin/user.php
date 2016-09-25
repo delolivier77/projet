a56e8cee2ac22ba0b5b6beceee05f7fcb636bee0
padawwweb <box@mailoo.org>
2016-09-25 14:23:16 +0200

    Fichier user (admin)

---
+++ app/Views/admin/user.php
@@ -0,0 +1,85 @@
+<?php
+
+// On charge le layout
+$this->layout('layout_admin', ['title' => 'Utilisateurs']);
+
+// On place le contenu de la vue
+$this->start('main_content');
+
+?>
+
+<!-- AFFICHAGE DE TOUS LES UTILISATEURS -->
+
+<h1>Tous les utilisateurs</h1>
+
+<!-- Affichage d'un message éventuel avec l'administrateur -->
+<?php echo $fmsg->display(); ?>
+
+<table class="table-striped table-bordered table-hover table-condensed">
+
+	<tr><th>id_u</th><th>Nom</th><th>Prénom</th><th>Email</th><th>Mot de passe</th><th>Date d'inscription</th><th>Statut</th><th colspan="2">Actions</th></tr>
+
+	<?php foreach($users as $valeur) : ?>	
+		<tr>
+			<?php foreach($valeur as $indice2 => $valeur2) : ?>
+				<td><?= $valeur2 ?></td>
+			<?php endforeach; ?>
+
+			<!-- Modification -->
+			<td><a href="<?php echo $this->url('admin_find_all_user_edit', array('id' => $valeur['id_u'])) ; ?>"><i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i></a></td>
+			<!-- Suppression -->
+			<td><a href="<?php echo $this->url('admin_delete_user', array('id' => $valeur['id_u'])) ; ?>" onclick="return confirm('Etes-vous sûr de vouloir supprimer cet utilisateur ?');"><i class="fa fa-trash-o fa-2x" aria-hidden="true"></i></a></td>
+		</tr>
+	<?php endforeach; ?>
+	
+</table>
+
+<?php
+// Par défaut, on sélectionne actif  dans le formulaire pour le statut : 
+$statut_a = (!isset($_POST['statut']) || (isset($_POST['statut'])) && $_POST['statut'] == "actif") ? 'selected' : '';  
+$statut_i = (isset($_POST['statut']) && $_POST['statut'] == "inactif") ? 'selected' : '';
+$statut_b = (isset($_POST['statut']) && $_POST['statut'] == "banni") ? 'selected' : '';
+?>
+
+<!-- FORMULAIRE -->
+
+<form action="<?php echo $this->url('admin_save_user') ?>" method="post">
+	
+	<h2>Gestion du statut actif / désactif / banni</h2>
+	
+	<input type="hidden" name="id_u" value="<?php if(isset($user)){ echo $user['id_u']; } ?>"/>
+		
+	<div class="form-group col-xs-4">
+
+		<label>Nom</label>
+		<input type="text" class="form-control" name="nom" placeholder="Nom" value="<?php if(isset($user)){ echo $user['nom']; } ?>"/>
+
+		<label>Statut</label>
+		<select name="statut" class="form-control">
+			<option value="actif" <?= $statut_a ?>>actif</option>
+			<option value="inactif" <?= $statut_i?>>inactif</option>
+			<option value="banni" <?= $statut_b?>>banni</option>
+		</select>
+		
+		<input type="submit" class="btn btn-primary" name="enregistrer" value="Enregistrer" />
+
+	</div>
+
+	<div class="form-group col-xs-4">
+
+		<label>Prénom</label>
+		<input type="text" class="form-control" name="prenom" placeholder="Prénom" value="<?php if(isset($user)){ echo $user['prenom']; } ?>"/>
+
+	</div>
+
+	<div class="form-group col-xs-4">
+
+		<label>Email</label>
+		<input type="email" class="form-control" name="email" placeholder="email" value="<?php if(isset($user)){ echo $user['email']; } ?>"/>
+
+	</div>
+
+</form>
+
+<?php $this->stop('main_content'); ?>
+<!-- fin du contenu de la vue -->
