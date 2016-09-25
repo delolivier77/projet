ecea0f429c5ef3bb5aff8314b0d31ddf322c3a25
padawwweb <box@mailoo.org>
2016-09-25 14:33:52 +0200

    MAJ DefaultController

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -1,18 +1,41 @@
 <?php
-
 namespace Controller;
 
 use \W\Controller\Controller;
+use \Controller\RechercheController;
+use \Model\StudentModel;
 
 class DefaultController extends Controller
 {
-
 	/**
-	 * Page d'accueil par défaut
-	 */
+	* Page d'accueil par défaut
+	*/
 	public function home()
 	{
-		$this->show('default/home');
+		// $rechercheController = new RechercheController();
+		// $rechercheResults = $rechercheController -> generateRechercheResults();
+		$studentModel = new StudentModel();
+		$lastStudents = $studentModel->getLastStudents();
+		
+		// $this->show('default/home',array('matieres' => $rechercheResults[0], 'scolarites' => $rechercheResults[1], 'lastStudentsForView'=>$lastStudents));
+
+
+		$this -> show('default/home', array('lastStudentsForView' => $lastStudents));
 	}
 
+	/**
+	* Page de contact
+	*/
+	public function contact()
+	{
+		$this->show('default/contact');
+	}
+
+	/**
+	* Page des conditions générales d'utilisation
+	*/
+	public function cgu()
+	{
+		$this->show('default/cgu');
+	}
 }
